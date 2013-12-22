26ca3bc77af412112c3035feb5e74119a87b259c
Swyter <690187+Swyter@users.noreply.github.com>
2013-12-22 23:20:22 +0100

    *set compile output to parent dir. *tweak the build script to accept commonly installed python paths,  recompile on key press and skip the pyc generation. *load them from process subdir.

--- msys/build_module.bat
+++ msys/build_module.bat
@@ -1,37 +1,40 @@
-@echo off
-python process_init.py
-python process_global_variables.py
-python process_strings.py
-python process_skills.py
-python process_music.py
-python process_animations.py
-python process_meshes.py
-python process_sounds.py
-python process_skins.py
-python process_map_icons.py
-python process_factions.py
-python process_items.py
-python process_scenes.py
-python process_troops.py
-python process_particle_sys.py
-python process_scene_props.py
-python process_tableau_materials.py
-python process_presentations.py
-python process_party_tmps.py
-python process_parties.py
-python process_quests.py
-python process_info_pages.py
-python process_scripts.py
-python process_mission_tmps.py
-python process_game_menus.py
-python process_simple_triggers.py
-python process_dialogs.py
-python process_global_variables_unused.py
-python process_postfx.py
-@del *.pyc
+@echo off && title compiling scaedumar 2 --
+set PATH="C:\Python24","C:\Python25";"C:\Python26";"C:\Python27";%PATH%
+set PYTHONPATH=%cd%;%cd%\data;%cd%\header;%cd%\id;%cd%\process
+:start
+cls
+python  -B -OO  process\process_init.py
+python  -B -OO  process\process_global_variables.py
+python  -B -OO  process\process_strings.py
+python  -B -OO  process\process_skills.py
+python  -B -OO  process\process_music.py
+python  -B -OO  process\process_animations.py
+python  -B -OO  process\process_meshes.py
+python  -B -OO  process\process_sounds.py
+python  -B -OO  process\process_skins.py
+python  -B -OO  process\process_map_icons.py
+python  -B -OO  process\process_factions.py
+python  -B -OO  process\process_items.py
+python  -B -OO  process\process_scenes.py
+python  -B -OO  process\process_troops.py
+python  -B -OO  process\process_particle_sys.py
+python  -B -OO  process\process_scene_props.py
+python  -B -OO  process\process_tableau_materials.py
+python  -B -OO  process\process_presentations.py
+python  -B -OO  process\process_party_tmps.py
+python  -B -OO  process\process_parties.py
+python  -B -OO  process\process_quests.py
+python  -B -OO  process\process_info_pages.py
+python  -B -OO  process\process_scripts.py
+python  -B -OO  process\process_mission_tmps.py
+python  -B -OO  process\process_game_menus.py
+python  -B -OO  process\process_simple_triggers.py
+python  -B -OO  process\process_dialogs.py
+python  -B -OO  process\process_global_variables_unused.py
+python  -B -OO  process\process_postfx.py
 echo.
 echo ______________________________
 echo.
 echo Script processing has ended.
-echo Press any key to exit. . .
-pause>nul
+echo Press any key to recompile. . .
+pause>nul && goto :start
